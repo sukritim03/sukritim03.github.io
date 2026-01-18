bb09e8dd13455de916637aebd013e524efc57189
sukritim03 <sukriti.mehra03@gmail.com>
2026-01-19 01:16:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/klenzo-clean-hub/",
   server: {
     host: "::",
     port: 8080,
